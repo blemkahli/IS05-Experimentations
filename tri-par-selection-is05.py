e44171d1363eaf6b173bab9af02df0bd56df03f6
JulieSampieri <118740189+JulieSampieri@users.noreply.github.com>
2024-04-30 12:23:30 +0200

    references et tri en python

---
+++ tri-par-selection-is05.py
@@ -0,0 +1,27 @@
+
+def tri_selection(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+def main():
+    # Lecture de references.txt
+    with open('C:/Users/julie/OneDrive/Documents/Hutech/GI04 P24/IS05/Experimentations_wsl/references-test.txt', 'r', encoding='UTF-8') as file:
+        references = [int(line.strip()) for line in file]
+
+    tri_selection(references)
+
+    # Écriture du fichier trié references-tri-selection.txt
+    with open('C:/Users/julie/OneDrive/Documents/Hutech/GI04 P24/IS05/Experimentations_wsl/references-tri-selection-test.txt', 'w') as file:
+        for reference in references:
+            file.write(str(reference) + '\n')
+    
+    print("Le fichier trié a été créé avec succès.")
+
+if __name__ == "__main__":
+    main()
+
---
+++ tri-par-tas-is05.py
@@ -0,0 +1,47 @@
+
+def construire_tas(arr, n, i):
+    largest = i  
+    l = 2 * i + 1     
+    r = 2 * i + 2    
+
+    if l < n and arr[i] < arr[l]:
+        largest = l
+
+    if r < n and arr[largest] < arr[r]:
+        largest = r
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]  
+        construire_tas(arr, n, largest)
+
+def tri_par_tas(arr):
+    n = len(arr)
+
+    # Construire un tas (heapify)
+    for i in range(n // 2 - 1, -1, -1):
+        construire_tas(arr, n, i)
+
+    # Extraire les éléments un par un du tas
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]  
+        construire_tas(arr, i, 0)
+
+
+
+def main():
+    # Lecture de references.txt
+    with open('C:/Users/julie/OneDrive/Documents/Hutech/GI04 P24/IS05/IS05-Experimentations/references-test.txt', 'r', encoding='UTF-8') as file:
+        references = [int(line.strip()) for line in file]
+
+    tri_par_tas(references)
+
+    # Écriture du fichier trié references-tri-selection.txt
+    with open('C:/Users/julie/OneDrive/Documents/Hutech/GI04 P24/IS05/IS05-Experimentations/references-tri-tas-test.txt', 'w') as file:
+        for reference in references:
+            file.write(str(reference) + '\n')
+    
+    print("Le fichier trié a été créé avec succès.")
+
+if __name__ == "__main__":
+    main()
+
